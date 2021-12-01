1a662f5bf69e225d846566564864f9e97a02de69
dGameBoy101b <rhysmader@gmail.com>
2021-12-01 16:33:13 +0800

    Added Passive Incomes
    Added expandable and shrinkable passive income table with total row.
    Refactored naming convention of form elements to use a dot separator for section prefixes.
    Refactored addRow, removeRow, and calcPerTimeTotal function to be more generalised.
    Changed time unit converter to be more accurate and less computation intensive.
    Added more documentation comments.

--- work hour calculator.js
+++ work hour calculator.js
@@ -1,16 +1,18 @@
-//Add a row to the expenses table
-function addExpense()
+//Add a row to the body of the table the given element is in
+function addRow(ele)
 {
-	const template_row = $('#expense-template').contents();
-	const table_body = $('#expense-body');
+	const table = $(ele).parents('table');
+	const template_row = table.find('template').contents();
+	const table_body = table.find('tbody');
 	var new_row = template_row.clone();
 	new_row.find('[name]').attr('name', function(i, val){ return val + table_body.children().length; });
 	table_body.append(new_row);
 }
-//Remove the expense row the given button is in
-function removeExpense(expense_button)
+//Remove the row the given element is in from the table body
+//Also changes the names of the elements in the remaining rows to be correctly indexed
+function removeRow(ele)
 {
-	var remove_row = $(expense_button).parents('tr');
+	var remove_row = $(ele).parents('tr').first();
 	const index = remove_row.find('[name]').first().attr('name').match(/[0-9]+$/)[0];
 	const table_body = remove_row.parents('tbody');
 	remove_row.remove();
@@ -26,60 +28,70 @@ function removeExpense(expense_button)
 		});
 	});
 }
-//Calculate the total listed expenses and output it
-function calcExpenseTotal()
+//Calculate the total amounts per their corresponding frequency listed under the given section name
+//Every element whose name is prefixed with the given section name thus matching /^section_name\./ is used as input or output
+//The total amount is outputted to an element suffixed with total-amount thus matching /^section_name\.total-amount$/
+//The total frequency is fetched from an element suffixed with total-frequency thus matching /^section_name\.total-frequency$/
+//Each listed item is assumed to have an amount and a frequency named as such with an index suffix matching /^section_name\.amount[0-9]+$/ and /^section_name\.frequency[0-9]+$/ respectively
+//See the convertTime function for the supported time frequencies
+function calcPerTimeTotal(section_name)
 {
-	if ($('[name^="expense"]:invalid').length > 0)
+	if ($('[name^="' + section_name + '"]:invalid').length > 0)
 	{
-		alert("Invalid expense data");
+		alert('Invalid ' + section_name + ' data');
 		return;
 	}
-	const total_output = $('[name="expense-total-amount"]');
-	const total_unit = $('[name="expense-total-unit"]').val();
-	const units = $('[name^="expense-unit"]').map(function(i, ele){ return ele.value; }).get();
-	const amounts = $('[name^="expense-amount"]').map(function(i, ele){ return parseFloat(ele.value); }).get();
+	const total_output = $('[name="' + section_name + '.total-amount"]');
+	const total_frequency = $('[name="' + section_name + '.total-frequency"]').val();
+	const frequencies = $('[name^="' + section_name + '.frequency"]').map(function(i, ele){ return ele.value; }).get();
+	const amounts = $('[name^="' + section_name + '.amount"]').map(function(i, ele){ return parseFloat(ele.value); }).get();
 	var total = 0;
-	for (const i in units)
+	for (const i in frequencies)
 	{
-		total += convertAmount(units[i], total_unit, amounts[i]);
+		total += convertTime(amounts[i], frequencies[i], total_frequency);
 	}
 	total_output.val(total);
 }
-//Convert given amount from given source unit to given destination unit
-function convertAmount(dst, src, val)
+//Convert given time from given source unit to given destination unit
+//Supported units: hour, day, week, fortnight, month, year
+function convertTime(val, dst, src)
 {
 	if (src === dst)
 	{
 		return val;
 	}
-	const units = ['hour', 'day', 'week', 'fortnight', 'month', 'year'];
-	const factors = [24, 7, 2, 2, 12];
-	var value = val;
-	const src_i = units.indexOf(src);
-	if (src_i < 0)
-	{
-		throw 'invalid source unit';
-	}
-	const dst_i = units.indexOf(dst);
-	if (dst_i < 0)
-	{
-		throw 'invalid destination unit';
-	}
-	if (src_i > dst_i)
-	{
-		for (const mod of factors.slice(dst_i, src_i))
-		{
-			value *= mod;
+	const dst_to_src = {
+		'hour':{
+			'day':24,
+			'week':24*7,
+			'fortnight':24*7*2,
+			'month':30*24,
+			'year':365*24
+		},
+		'day':{
+			'week':7,
+			'fortnight':7*2,
+			'month':365/12,
+			'year':365
+		},
+		'week':{
+			'fortnight':2,
+			'month':365/12/7,
+			'year':365/7
+		}, 
+		'fortnight':{
+			'month':365/12/7/2,
+			'year':365/7/2
+		}, 
+		'month':{
+			'year':12
 		}
-	}
-	else
+	};
+	if (src in dst_to_src && dst in dst_to_src[src])
 	{
-		for (const mod of factors.slice(src_i, dst_i))
-		{
-			value /= mod;
-		}
+		return val / dst_to_src[src][dst];
 	}
-	return value;
+	return val * dst_to_src[dst][src];
 }
 //Trim trailing and leading zeros from given input field
 function trimZeros(field)
